5e5da38015cf7562cec66dc2fab82439218f355a
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-05-17 02:16:33 +0600

    Frontend# Included redux storage and hooks, axios, react router.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { addInterceptors } from './axios';
+import { persistor, store } from './app/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import DialogsProvider from './components/Dialogs/DialogsProvider';
+
+addInterceptors(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <DialogsProvider>
+          <App />
+        </DialogsProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+);
