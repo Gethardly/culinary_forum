40f34cebfd0a5409c99778b96aee1c27f2f567fa
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-05-24 05:57:32 +0600

    Backend# Added checking of the field name and save the files with the same name folder.

--- backend/multer.ts
+++ backend/multer.ts
@@ -6,9 +6,19 @@ import config from './config';
 
 const imageStorage = multer.diskStorage({
   destination: async (_req, _file, cb) => {
-    const destDir = path.join(config.publicPath, 'images/');
+    const destDir = path.join(config.publicPath, 'images');
+    const avatarsDir = path.join(destDir, 'avatars');
+    const recipePhotosDir = path.join(destDir, 'recipe_photos');
+
     await fs.mkdir(destDir, { recursive: true });
-    cb(null, destDir);
+    await fs.mkdir(avatarsDir, { recursive: true });
+    await fs.mkdir(recipePhotosDir, { recursive: true });
+
+    if (_file.fieldname === 'avatar') {
+      cb(null, avatarsDir);
+    } else if (_file.fieldname === 'photoGallery') {
+      cb(null, recipePhotosDir);
+    }
   },
   filename: (_req, file, cb) => {
     const extension = path.extname(file.originalname);
