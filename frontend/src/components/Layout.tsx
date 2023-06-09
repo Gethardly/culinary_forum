dbddc567e6b4c987ebea2a155f05d1bd62fe8816
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-06-10 05:11:56 +0600

    Added hiding appBar possibility.

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,16 +1,20 @@
 import React, { PropsWithChildren, ReactNode } from 'react';
 import AppToolbar from './AppToolbar/AppToolbar';
 import { Box, Container, CssBaseline } from '@mui/material';
+import { useAppSelector } from '../app/hooks';
+import { hideAppBar, selectAppBar } from './AppToolbar/AppToolBarSlice';
 
 interface Props extends PropsWithChildren {
   children: ReactNode;
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
+  hideAppBar(false);
+  const hideAppbar = useAppSelector(selectAppBar);
   return (
     <>
       <CssBaseline />
-      <AppToolbar />
+      {hideAppbar ? '' : <AppToolbar />}
       <Box component="main">
         <Container maxWidth={false}>{children}</Container>
       </Box>
